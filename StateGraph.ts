a368850e85bc2a8678585d54e7ad82454b309ae4
Meddan <christoffer.medin@gmail.com>
2016-05-20 13:16:33 +0200

    Created StateGraph.ts, looking good.

---
+++ StateGraph.ts
@@ -0,0 +1,55 @@
+///<reference path="Graph.ts"/>
+///<reference path="World.ts"/>
+// This is an example graph
+// consisting of a 2-dimensional grid
+// where neighbors are vertical and horisontal
+
+
+class StateNode {
+    constructor(
+        public state : WorldState
+    ) {}
+
+    add(action : String) : GridNode {
+        /*return new GridNode({
+            x: this.pos.x + delta.x,
+            y: this.pos.y + delta.y
+        });*/
+        //should probably return: state.doAction(action)
+        return null;
+    }
+
+    compareTo(other : StateNode) : number {
+        //return (this.pos.x - other.pos.x) || (this.pos.y - other.pos.y);
+        return 0;
+    }
+
+    toString() : string {
+        return state.toString;
+    }
+}
+
+
+class StateGraph implements Graph<StateNode> {
+
+    constructor(
+      startingState : WorldState
+      //some form of representing the endstate
+    ) {
+      //shit to do in constructor goes here
+     }
+
+    outgoingEdges(node : StateNode) : Edge<StateNode>[] {
+        var outgoing : Edge<StateNode>[] = [];
+        //Add all reachable states (with one action) to outgoing
+        return outgoing;
+    }
+
+    compareNodes(a : StateNode, b : StateNode) : number {
+        return a.compareTo(b);
+    }
+
+    toString(start? : GridNode, goal? : (n:GridNode) => boolean, path? : GridNode[]) : string {
+        return "nope";
+    }
+}
